b5effd05eb464e00bf6942361b6b13a3f4daf68f
ktmrlky <smhktmrlky@gmail.com>
2020-07-19 17:05:11 +0300

    last commit

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
 import Spinner from '../ui/spinner';
-import Footer from '../ui/Footer';
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
